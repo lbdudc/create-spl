01cb85356d5dad79e92b3b593bc66551793ca6fb
vlamas <vlamas@udc.es>
2025-07-23 11:10:46 +0200

    feat: Improve error handling in derivation engine and update model import paths for consistency

--- templates/template-base/src/derivationEngine.js
+++ templates/template-base/src/derivationEngine.js
@@ -3,16 +3,22 @@ import { splModulesConfig } from "./splConfig.js";
 import { SPL_MODULE_TRANSF_FOLDER_NAME, SPL_MODULE_EXTRA_FOLDER_NAME, FM_FILENAME } from "./consts/index.js";
 import path from "path";
 
-
 const VERBOSE = false;
 const sep = path.sep;
 
-let featureModel = `.${sep}${FM_FILENAME}`;
-if (!featureModel.endsWith("uvl")) {
-    featureModel = readFile(featureModel)
+const featureModel = `.${sep}${FM_FILENAME}`;
+
+const mainComponent = splModulesConfig[0];
+
+if (!mainComponent) {
+    throw new Error("No main component found in the SPL modules configuration.");
 }
 
-const mainComponent = splModulesConfig.find(component => component.isMain);
+const splModulesConfigComponents =
+    splModulesConfig.reduce((acc, component) => {
+        acc[component.name] = component.path;
+        return acc;
+    }, {});
 
 const createEngine = new DerivationEngine({
     codePath: null,
@@ -21,7 +27,7 @@ const createEngine = new DerivationEngine({
     extraJS: readFile(mainComponent.path + `${sep}${SPL_MODULE_EXTRA_FOLDER_NAME}`),
     modelTransformation: readFile(mainComponent.path + `${sep}${SPL_MODULE_TRANSF_FOLDER_NAME}`),
     verbose: VERBOSE,
-    components: splModulesConfig
+    components: splModulesConfigComponents
 });
 
 
